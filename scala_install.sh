4e4f2cc6b10c9780a0bc548a90c140280defebcd
zhuangqunxiong <qxiong133@163.com>
2015-03-09 14:04:35 +0800

    install scala script

---
+++ scala_install.sh
@@ -0,0 +1,6 @@
+
+apt-get install -y openjdk-7-jdk
+apt-get install -y scala
+
+#wget http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.0/sbt.deb --no-verbose
+#dpkg -i sbt.deb
