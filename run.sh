23b724917372be43a9c0ece8968cadf4ea474368
root <root@scrapy2.dkhs.com>
2015-03-20 09:49:10 +0800

    add compile parameters

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 cd ..
-sbt package && ~/spark-1.2.0-bin-hadoop2.4/bin/spark-submit --class "SimpleApp" --master local[4] target/scala-2.10/simple-project_2.10-1.0.jar localhost 9999
+sbt -J-Xss1m -J-Xms256m -J-Xmx512m -J-XX:+CMSClassUnloadingEnabled package && ~/spark-1.2.0-bin-hadoop2.4/bin/spark-submit --class "SimpleApp" --master local[4] target/scala-2.10/simple-project_2.10-1.0.jar localhost 9999
